7e2b3c015693666c55d59236ec68031b76f38ec5
nekogakure <156896971+nekogakure@users.noreply.github.com>
2025-11-01 13:38:27 +0900

    upd test

--- src/kernel/tests/run.c
+++ src/kernel/tests/run.c
@@ -6,44 +6,42 @@ void run_test() {
         #ifdef MEM_TEST
         printk("> MEMORY TEST\n");
         mem_test();
-        #endif
-
         new_line();
+        #endif
 
         #ifdef INTERRUPT_TEST
         printk("> INTERRUPT TEST\n");
         interrupt_test();
-        #endif
-
         new_line();
+        #endif
 
         #ifdef INTERRUPT_VECTOR_TEST
         printk("> INTERRUPT VECTOR TEST\n");
         interrupt_vector_test();
-        #endif
-
         new_line();
+        #endif
 
         #ifdef ALLOC_IRQ_TEST
         printk("> ALOOC IRQ TEST\n");
         alloc_irq_test();
-        #endif
-
         new_line();
+        #endif
 
         #ifdef GDT_TEST
+        printk("> GDT TEST");
         gdt_test();
-        #endif
-
         new_line();
+        #endif
 
         #ifdef PAGING_TEST
         printk("> PAGING TEST\n");
         paging_test();
+        new_line();
         #endif
 
         #ifdef VMEM_TEST
         printk("> VMEM TEST\n");
         vmem_test();
+        new_line();
         #endif
 }
